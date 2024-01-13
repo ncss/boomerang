d4e6283cbe92cf66e19e02de7b02742171c540d9
Joel Gibson <joel@jgibson.id.au>
2024-01-13 13:36:46 +1100

    API returns empty object

--- server_test.py
+++ server_test.py
@@ -27,22 +27,19 @@ class ServerTestCase(unittest.TestCase):
         res = self.client.get(PATH)
         self.assertEqual(res.status_code, 404)
         res_json = res.get_json()
-        self.assertIn("error", res_json)
-        self.assertIn("status", res_json)
+        self.assertEqual(res_json, {})
 
         # deleting should be 404
         res = self.client.delete(PATH)
         self.assertEqual(res.status_code, 404)
         res_json = res.get_json()
-        self.assertIn("error", res_json)
-        self.assertIn("status", res_json)
+        self.assertEqual(res_json, {})
 
         # now we create it
         res = self.client.post(PATH, json=data)
         self.assertEqual(res.status_code, 200)
         res_json = res.get_json()
-        self.assertEqual(res_json["value"]["one"], 1)
-        self.assertEqual(res_json["key"], KEY)
+        self.assertEqual(res_json["one"], 1)
 
         # now it should exist
         res = self.client.get(PATH)
@@ -54,14 +51,13 @@ class ServerTestCase(unittest.TestCase):
         res = self.client.delete(PATH)
         self.assertEqual(res.status_code, 200)
         res_json = res.get_json()
-        self.assertEqual(res_json, {'key': KEY, 'deleted': True})
+        self.assertEqual(res_json, {})
 
         # now it should no longer exist
         res = self.client.get(PATH)
         self.assertEqual(res.status_code, 404)
         res_json = res.get_json()
-        self.assertIn("error", res_json)
-        self.assertIn("status", res_json)
+        self.assertEqual(res_json, {})
 
     def test_updates(self):
         data = {"number": 1}
@@ -75,29 +71,29 @@ class ServerTestCase(unittest.TestCase):
         # now create it
         res = self.client.post(PATH, json=data)
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.get_json()["value"]["number"], 1)
+        self.assertEqual(res.get_json(), data)
 
         # check that we can retrieve it correctly
         res = self.client.get(PATH)
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.get_json()["number"], 1)
+        self.assertEqual(res.get_json(), data)
 
         # now post to it again
         data = {"number": 42}
         res = self.client.post(PATH, json=data)
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.get_json()["value"]["number"], 42)
+        self.assertEqual(res.get_json(), data)
 
         # now post to it yet again
         data = {"number": 123}
         res = self.client.post(PATH, json=data)
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.get_json()["value"]["number"], 123)
+        self.assertEqual(res.get_json(), data)
 
         # check that it has actually been updated
         res = self.client.get(PATH)
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.get_json()["number"], 123)
+        self.assertEqual(res.get_json(), data)
 
     def test_invalid_input(self):
         # not json
